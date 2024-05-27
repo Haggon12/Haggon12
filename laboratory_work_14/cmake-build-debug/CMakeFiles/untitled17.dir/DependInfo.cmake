6f5192acb485910b54d7bb33063c4e0d493e3d1f
Haggon12 <167657398+Haggon12@users.noreply.github.com>
2024-05-27 11:49:21 +0300

    Add files via upload

---
+++ laboratory_work_14/cmake-build-debug/CMakeFiles/untitled17.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/zolot/Downloads/laboratory_work_14/main.c" "CMakeFiles/untitled17.dir/main.c.obj" "gcc" "CMakeFiles/untitled17.dir/main.c.obj.d"
+  "C:/Users/zolot/Downloads/laboratory_work_14/test vector.c" "CMakeFiles/untitled17.dir/test_vector.c.obj" "gcc" "CMakeFiles/untitled17.dir/test_vector.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ laboratory_work_14/cmake-build-debug/CMakeFiles/untitled17.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled17.dir/main.c.obj"
+  "CMakeFiles/untitled17.dir/main.c.obj.d"
+  "CMakeFiles/untitled17.dir/test_vector.c.obj"
+  "CMakeFiles/untitled17.dir/test_vector.c.obj.d"
+  "libuntitled17.dll.a"
+  "untitled17.exe"
+  "untitled17.exe.manifest"
+  "untitled17.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/untitled17.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
